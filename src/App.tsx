6107245b801c5b980fc076ad453801203623586f
Ryan Crockett <ryan.crockett8@gmail.com>
2021-03-28 13:08:06 -0400

    Add app logo, title, description

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 
+import { ReactComponent as Logo } from './assets/images/AirLogo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="app">
-      <header className="app-header">
-        {/*  */}
+export const App = React.memo(() => (
+  <main className="app">
+    <section className="app__logo">
+      <figure>
+        <Logo title="Air Logo" className="app__logo-image" />
+      </figure>
+    </section>
+    <section className="app__content">
+      <header>
+        The Person Finder
       </header>
-    </div>
-  );
-}
+      <p className="app__description">
+        If you just can’t find someone and need to know what they look like, you’ve come to the right place! Just type the name of the person you are looking for below into the search box!
+      </p>
+    </section>
 
-export default App;
+  </main>
+));
+
+App.displayName = 'App';
